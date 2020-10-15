116bfa4298439670dea7320251542225e861363d
Luis Benet <benet@icf.unam.mx>
2020-10-15 17:59:38 -0500

    Arreglos en jl_to_ipynb.jl

--- jl_to_ipynb.jl
+++ jl_to_ipynb.jl
@@ -1,13 +1,13 @@
 # jl_to_ipynb.jl
 #
-# Correr desde la línea de comandos
-# > julia jl_to_ipynb.jl 1-IntrodJulia.jl
+# Correr desde la línea de comandos:
+# 
+#     julia jl_to_ipynb.jl clases/1-IntrodJulia.jl
 #
 # Esto producirá el archivo "1-IntrodJulia.ipynb" en el directorio "notebooks/"
 #
 
-file = "./clases/" * basename(ARGS[1])
-@show(file)
+file = basename(ARGS[1])
 
 # Verificando que es un archivo con extensión `.jl`
 @assert file[end-2:end] == ".jl" """\n
@@ -15,10 +15,9 @@ Error: El archivo debe ser un archivo con extensión `.jl`
 """
 
 using Pkg
-Pkg.activate(".")
+Pkg.activate(".");
 
 using Literate
-Literate.notebook(file; execute=false)
-outfile = ARGS[1][1:end-3] * ".ipynb"
-mv(outfile, "./notebooks/" * outfile)
+outdir = pwd() * "/notebooks/"
+Literate.notebook("./clases/" * file, outdir; execute=false)
 println("Done")
