2b766db903efd5e2633fd5a84bb5aa7ffe8ead67
Erasmo98 <erasmo@ciencias.unam.mx>
2021-01-25 22:45:34 -0600

    Añado pruebas y retiro archivos.

--- tareas/Tarea_Miguel_Erasmo/taylor.jl
+++ tareas/Tarea_Miguel_Erasmo/taylor.jl
@@ -1,5 +1,5 @@
 module SeriesTaylor
-export Taylor
+export Taylor, trig
 
 """
 Definición de polinomios de Taylor, donde
@@ -17,7 +17,7 @@ struct Taylor{T <: Number}
     Taylor(orden, polim) = new{eltype(polim)}(orden, vcat( polim, zeros(eltype(polim), orden + 1 - length(polim) ) ) )
 end
 
-import Base: +, -, *, /, inv, ==, exp, log, sin, cos, ^
+import Base: +, -, *, /, inv, ==, exp, log, sin, cos, ^, isapprox
 
 +(T::Taylor) = T
 +(c::Number, T::Taylor) = Taylor( vcat([T.polim[1] + c], T.polim[2:end]) )
@@ -38,6 +38,7 @@ inv(T::Taylor) = Taylor(T.orden, [one( eltype(T.polim) )]) / T
 /(T::Taylor, c::Number) = Taylor( T.polim / c )
 
 ==(T1::Taylor, T2::Taylor) = T1.polim == T2.polim
+isapprox(T1::Taylor, T2::Taylor) = T1.polim ≈ T2.polim
 
 function *(T1::Taylor, T2::Taylor)
     polim = eltype( promote(T1.polim[1], T2.polim[1]) )[]
@@ -62,16 +63,16 @@ function /(T1::Taylor, T2::Taylor)
     return Taylor(polim)
 end
 
-function ^(T::Taylor, n::Int)
-    n == 0 && return Taylor(T.orden, [one( eltype(T.polim) )])
-    n == 1 && return T
-    
-    if n > 1
-        T * T^(n - 1)
-    elseif n < 0
-        (inv(T))^abs(n)
-    end
-end
+# function ^(T::Taylor, n::Int)
+#     n == 0 && return Taylor(T.orden, [one( eltype(T.polim) )])
+#     n == 1 && return T
+# 
+#     if n > 1
+#         T * T^(n - 1)
+#     elseif n < 0
+#         (inv(T))^abs(n)
+#     end
+# end
 
 function exp(T::Taylor)
     polim = [exp(T.polim[1])]
@@ -96,12 +97,34 @@ end
 
 function ^(T::Taylor, α)
     p0 = T.polim[1]
-    polim = [p0^α]
+    orden = T.orden
     
-    for i in 2:T.orden+1
-        push!(polim, inv((i-1)*p0) * sum([α*(i-1-j)-j for j in 0:i-2] .* reverse(T.polim[2:i]) .* polim[1:i-1]) )
+    if p0 == 0 && α == Int(α) && α>0
+        orden_min = zero(Int)
+        
+        for i in 2:orden+1
+            if T.polim[i] != 0
+                orden_min = i-1
+                break
+            end
+        end
+        
+        nuevo_orden_min = α * orden_min
+        if orden_min == 0 || nuevo_orden_min > orden
+            polim = [zero(eltype(T))]
+        else
+            T_aux = Taylor(T.polim[orden_min+1:end])^α
+            polim = vcat(zeros(eltype(T_aux.polim), nuevo_orden_min), T_aux.polim[1:orden + 1 - nuevo_orden_min])
+        end
+    else
+    
+        polim = [p0^α]
+
+        for i in 2:T.orden+1
+            push!(polim, inv((i-1)*p0) * sum([α*(i-1-j)-j for j in 0:i-2] .* reverse(T.polim[2:i]) .* polim[1:i-1]) )
+        end
     end
-    
+
     return Taylor(polim)
 end
 
